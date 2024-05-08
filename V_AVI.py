db4ff7b7ee52740f09d46d68183a10869443bd86
Vũ Quang Đại (Bevis Vũ) <127699283+VQD-BSV@users.noreply.github.com>
2024-05-08 15:43:22 +0700

    Add files via upload

---
+++ V_AVI.py
@@ -0,0 +1,17 @@
+import os, binascii
+
+with open("REF.AVI", "rb") as r:
+	data = r.read()
+
+with open("AVSEQ02 (45).AVI", "rb") as r:
+	data_2 = r.read()
+
+
+header = data.find(binascii.unhexlify('FFFFFFFFFF'))
+data_fix = data_2.find(binascii.unhexlify('FFFFFFFFFF'))
+
+with open("HEX", "wb") as w:
+	w.write(data[:header - 16])
+
+with open("HEX", "ab") as a:
+	a.write(data_2[data_fix - 16:])
