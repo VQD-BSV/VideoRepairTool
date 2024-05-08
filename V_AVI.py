d4037cc4e6ccb7a4707d17ea987dbb3d8110c7a5
Vũ Quang Đại (Bevis Vũ) <127699283+VQD-BSV@users.noreply.github.com>
2024-05-08 19:09:48 +0700

    Update V_AVI.py

--- V_AVI.py
+++ V_AVI.py
@@ -10,7 +10,4 @@ header = data.find(binascii.unhexlify('FFFFFFFFFF'))
 data_fix = data_2.find(binascii.unhexlify('FFFFFFFFFF'))
 
 with open("HEX", "wb") as w:
-	w.write(data[:header - 16])
-
-with open("HEX", "ab") as a:
-	a.write(data_2[data_fix - 16:])
+	w.write(data[:header - 16] + data_2[data_fix - 16:])
